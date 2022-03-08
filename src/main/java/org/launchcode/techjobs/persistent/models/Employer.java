1bcc7613074f33facaaad1e8c30928eb1df3ee91
Ingrid Barrett <barrett92@yahoo.com>
2022-03-08 12:54:44 -0500

    2nd commit - Part two Models updated, but questions on validation on location field

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,22 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    private String location;
 
+    public Employer(String location) {
+        this.location = location;
+    }
+    public Employer() { }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,25 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Max;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Max(255)
+    private String skill;
+
+    public Skill(String skill) {
+        this.skill = skill;
+    }
+
+    public Skill() { }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
 }
